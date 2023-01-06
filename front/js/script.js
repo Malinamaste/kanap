ce003f5eeb88ec0957cd39ff72b2e00d2c003229
Melanie Morey <morey.melanie@gmail.com>
2023-01-06 14:07:25 +0100

    script.js amélioré

--- front/js/script.js
+++ front/js/script.js
@@ -1,40 +1,55 @@
+main()
+
 /*-------------------------------------------------------------------
-    1ère requête pour récupérer les données des produits sur l'API
+    Création d'une fonction main() qui contient le code de base
+            et s'execute dès le chargement de la page
 --------------------------------------------------------------------*/
-fetch("http://localhost:3000/api/products")
-  // récupération du résultat de la requête au format json (Promise)
-  .then(function(res) {
-    if (res.ok) {
-      return res.json();
+
+async function main() {
+    // on récupère les produits
+    const products = await getProducts()
+    //console.log(products)
+
+    // on boucle pour afficher tous les produits
+    for (product of products) {
+        displayProduct(product)
     }
-  })
-  // on retourne et récupère la vraie valeur du résultat json précédent (Promise)
-  .then(function(objectKanap) {
-    console.log(objectKanap);
-    // call de la fonction d'affichage des produits
-    displayKanaps(objectKanap);
-  })
-  // si erreur, on remplace le contenu h1 par erreur 404 et on affiche l'erreur dans la console
-  .catch(function(err) {
-    document.querySelector(".titles").innerHTML = "<h1>erreur 404</h1>";
-    console.log("erreur 404 :" + err);
-  });
+}
+
 /*-------------------------------------------------------------------
-    Fonction d'affichage des produits de l'API sur la page index
+    Création d'une fonction qui fait office de première requête
+        pour récupérer les données des produits sur l'API
 --------------------------------------------------------------------*/
-function displayKanaps(index) {
-  // variable relative à la section#items (DOM)
-  let sectionKanap = document.getElementById('items');
-  // boucle pour chaque article > section#items de la page index
-  for (let article of index) {
-    // création et ajout des zones d'articles
-    sectionKanap.innerHTML += //innerHTML injecte le nouveau contenu
-    `<a href="./product.html?_id=${article._id}">
+
+function getProducts() {
+    return fetch("http://localhost:3000/api/products")
+        // récupération du résultat de la requête au format json (Promise)
+        .then(function(httpBodyResponse) {
+            return httpBodyResponse.json()
+        })
+        // on retourne et récupère la vraie valeur du résultat json précédent (Promise)
+        .then(function(products) {
+            //console.log(products)
+            return products
+        })
+        // au cas où l'API serait down
+        .catch(function(error) {
+            alert(error)
+        })
+}
+
+/*---------------------------------------------------------------------------------
+    Création d'une fonction d'affichage d'un produit de l'API sur la page index
+----------------------------------------------------------------------------------*/
+
+function displayProduct(product) {
+    //.innerHTML injecte le nouveau contenu
+    document.getElementById('items').innerHTML += 
+    `<a href="./product.html?_id=${product._id}">
         <article>
-            <img src="${article.imageUrl}" alt="${article.altTxt}">
-            <h3 class="productName">${article.name}</h3>
-            <p class="productDescription">${article.description}</p>
+            <img src="${product.imageUrl}" alt="${product.altTxt}">
+            <h3 class="productName">${product.name}</h3>
+            <p class="productDescription">${product.description}</p>
         </article>
     </a>`;
-  }
 }
